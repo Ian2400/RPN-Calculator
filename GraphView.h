f1de5b9d391b9db1cab6b62b0517ce66008b55fd
Ian Pytlarz <Ian2400@gmail.com>
2012-07-16 21:36:45 -0400

    Added a Graphview class and a UI Navigation Controller with the CalculatorViewController as the root view

---
+++ GraphView.h
@@ -0,0 +1,13 @@
+//
+//  GraphView.h
+//  RPN Calculator
+//
+//  Created by Ian Pytlarz on 7/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphView : UIView
+
+@end
---
+++ GraphView.m
@@ -0,0 +1,31 @@
+//
+//  GraphView.m
+//  RPN Calculator
+//
+//  Created by Ian Pytlarz on 7/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "GraphView.h"
+
+@implementation GraphView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
